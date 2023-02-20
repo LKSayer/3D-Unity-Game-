b15d74c582fac2edef5a11507b6a5e054f8c4772
Liam Sayer <59685156+e420@users.noreply.github.com>
2023-02-20 22:15:39 +1300

    (Unfinished) Timer included

---
+++ 3D Game/Assets/Scripts/TimerText.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+public class TimerText : MonoBehaviour
+{
+    [SerializeField] Text Timer;
+    private float startTime;
+    void Start()
+    {
+        startTime = Time.time;
+    }
+    
+    void update()
+    {
+        float t = Time.time - startTime;
+
+        string minutes = ((int) t / 60 ).ToString();
+        string seconds = (t % 60).ToString("f2");
+
+        Timer.text = minutes + ":" + seconds;
+    }
+    
+}
